69190577a8de2817ac0be1e718cfa5ba965d705a
Emilio Escobar <eescobar@gmail.com>
2020-03-31 19:53:46 -0700

    remove app config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
