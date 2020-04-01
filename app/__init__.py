c13e634fb14f4ef8879de3ea4ac82b5bb0c15440
Emilio Escobar <eescobar@gmail.com>
2020-03-31 20:02:28 -0700

    just use single file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,1 @@
-import os
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
-
-app = Flask(__name__)
-#app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-heroku = Heroku(app)
-db = SQLAlchemy(app)
--- app/main.py
+++ app/main.py
@@ -7,10 +7,18 @@ from flask import jsonify
 from flask import request
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 
-from .models import CTIContact
-from . import app
+from sqlalchemy.dialects.postgresql import JSONB
 
+app = Flask(__name__)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+## helper functions
 def build_response(message):
     resp = { "blocks" : [
         { "type" : "section",
@@ -21,6 +29,28 @@ def build_response(message):
     ]}
     return resp
 
+##ORM
+class CTIContact(db.Model):
+    __tablename__ = 'cti_contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(JSONB)
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+            return '<id {}>'.format(self.id)
+
+class CTIHelp(db.Model):
+        __tablename__ = 'cti_help'
+        id = db.Column(db.Integer, primary_key=True)
+        data = db.Column(JSONB)
+
+        def __init__(self, data):
+                sefl.data = data;
+
+        def __repr__(self):
+                return '<id {}>'.format(self.id)
 
 ##routes
 
